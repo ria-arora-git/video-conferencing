0675765f301a005f85afe279ca9562216a3a2e53
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-03-19 20:31:45 +0530

    clerk and stream connected

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
     <main>
+    <StreamVideoProvider>
        {children}
+    </StreamVideoProvider>
     </main>
   )
 }
 
-export default RootLayout
+export default RootLayout;
--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,27 +1,42 @@
-// import { StreamCall, StreamVideo, StreamVideoClient, User, } from "@stream-io/video-react-sdk";
-// import { useEffect } from "react";
-  
-//   const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+'use client';
 
+import { ReactNode, useEffect, useState } from 'react';
+import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
 
-  
-//   export const StreamVideoProvider = ({children} : { children : ReactNode }) => {
-    
-//     const [videoClient, setVideoClient] = useState<StreamVideoClient>();
-//     const { user,isLoaded } = useUser();
+import { tokenProvider } from '@/actions/stream.actions';
+import Loader from '@/components/Loader';
 
-//     useEffect(() => {
-//       const client = new StreamVideoClient(apiKey);
-//       setVideoClient(client);
-//     }
+const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-//     return (
-        
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
 
-//       <StreamVideo client={videoClient}>
-     
-//       </StreamVideo>
-//     );
-//   };
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!API_KEY) throw new Error('Stream API key is missing');
 
-// export default StreamVideoProvider;
+    const client = new StreamVideoClient({
+      apiKey: API_KEY,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
+
+    setVideoClient(client);
+  }, [user, isLoaded]);
+
+  if (!videoClient) return <Loader />;
+
+  return (
+  <StreamVideo client={videoClient}>
+    {children}
+  </StreamVideo>
+  );
+};
+
+export default StreamVideoProvider;
