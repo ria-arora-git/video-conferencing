94bcb44aba09dfce87349b9728566fa856d78134
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-06-27 23:45:28 +0530

    time fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-  title: "Reunivo",
+  title: "Univo",
   description: "App for virtual meetings",
   icons: {
     icon: "/icons/favicon.png"
